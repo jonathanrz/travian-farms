0b678f66b336b129bd229db5caa1b82e3fececfb
Jonathan Rafael Zanella <jonathanrafaelz@gmail.com>
2017-09-24 18:18:34 -0300

    Generate attack times

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ const farms = [
     localization: "-43|24",
     distance: 2,
     time: 17,
+    time_to_attack: 45,
     troops: {
       type: "Guardião das Cinzas",
       amount: 5
@@ -32,6 +33,19 @@ class App extends Component {
       farms,
       troops,
     };
+
+    this.generateTime = this.generateTime.bind(this)
+  }
+
+  generateTime(farm) {
+      let times = [farm.time]
+      let currentTime = farm.time
+      while(currentTime < 720) {
+        currentTime += farm.time_to_attack + Math.floor((Math.random() * 5) + 1)
+        times.push(currentTime)
+      }
+
+      return times.join(",")
   }
 
   render() {
@@ -40,6 +54,7 @@ class App extends Component {
         <FarmTable
           list={farms}
           onDismiss={ function() {} }
+          generateTime={ this.generateTime }
         />
       </div>
     );
@@ -48,25 +63,51 @@ class App extends Component {
 
 class FarmTable extends Component {
   render() {
-    const { list, onDismiss } = this.props;
+    const { list, onDismiss, generateTime } = this.props;
     return (
       <div className="table">
+        <div key="0" className="table-row">
+          <span style={{ width: '15%' }}>
+            Nome
+          </span>
+          <span style={{ width: '8%' }}>
+            Localização
+          </span>
+          <span style={{ width: '7%' }}>
+            Distância
+          </span>
+          <span style={{ width: '10%' }}>
+            Tempo necessário
+          </span>
+          <span style={{ width: '15%' }}>
+            Tropa
+          </span>
+          <span style={{ width: '35%' }}>
+            Tempo para ataques
+          </span>
+          <span style={{ width: '10%' }}>
+            Ações
+          </span>
+        </div>
         { list.map(item =>
           <div key={item.id} className="table-row">
-            <span style={{ width: '40%' }}>
+            <span style={{ width: '15%' }}>
               {item.name}
             </span>
-            <span style={{ width: '10%' }}>
+            <span style={{ width: '8%' }}>
               {item.localization}
             </span>
-            <span style={{ width: '10%' }}>
+            <span style={{ width: '7%' }}>
               {item.distance}
             </span>
             <span style={{ width: '10%' }}>
-              {item.time}
+              {item.time} - {item.time_to_attack}
             </span>
-            <span style={{ width: '20%' }}>
+            <span style={{ width: '15%' }}>
               {item.troops.amount} {item.troops.type}
+            </span>
+            <span style={{ width: '35%' }}>
+              {generateTime(item)}
             </span>
             <span style={{ width: '10%' }}>
               <button
